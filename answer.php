c380afb4623829dbd8e9e533a3fd41584f25e2bf
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-06 20:59:07 +0000

    edited php

--- answer.php
+++ answer.php
@@ -3,18 +3,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Movie Age Rating, in PHP" />
+  <meta name="description" content="Welcome to the Museum, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.orange-green.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-green.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Movie Age Rating, in PHP</title>
+  <title>Welcome to the Museum in PHP</title>
 </head>
 
 <body>
@@ -22,12 +22,12 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Movie Age Rating, in PHP</span>
+        <span class="mdl-layout-title">Welcome to the Museum! in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/movie_ratings.jpg" alt="movie_ratings" width="250" />
+        <img src="./images/museum.png" alt="museum" width="250" />
       </div>
       <div class="page-content-php">
         <div name="age-entered">
@@ -38,22 +38,18 @@
             <div class="mdl-layout__left">
               <?php
 
+              //input
               $age = $_POST["age-entered"];
+              $dayTuesdayChecked = $_POST["option-tuesday"];
+              $dayThursdayChecked = $_POST["option-thursday"];
+              $day = $_POST = ["day-selected"];
 
               // process
-              if ($age >= 17) {
+              if (($dayTuesdayChecked == true || $dayThursdayChecked == true) || ($age > 12 && $age < 21)) {
                 // output
-                echo "You are " . $age . " so you can watch an R rated movie alone.";
-                //process
-              } elseif ($age >= 13) {
-                // output
-                echo "You are " . $age . " so you can watch a PG-13 movie alone.";
-              } elseif ($age >= 9) {
-                // output
-                echo "You are " . $age . " so you can watch a G or PG rated movie alone.";
-              } elseif ($age >= 6) {
-                // output
-                echo "You are " . $age . " so you can watch a G rated movie alone.";
+                echo "The day is " . $day . " and you are " . $age . " so you get the student discount.";
+              } else {
+                echo "The day is ". $day . " and you are " . $age . " so you have to pay the regular price.";
               }
 
               ?>
